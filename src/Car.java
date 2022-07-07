5398ff953f7df4281bcdc36aebe079a36408eaa1
panterpower <lider.svb@gmail.com>
2022-07-07 20:16:00 +0800

    Добавил переменные - детали машины

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,17 @@
 public abstract class Car {
+    int wheel;
+    double gastank;
+    boolean engine = true;
+    boolean electrics = true;
+    boolean headlights = true;
+
+
+
+
+//    Машины состоят из следующих компонентов: 4 колеса, бензобак, двигатель, электрика, фары
+//    Каждое колесо имеет состояние - проколото или нет, диаметр (camry - 17, solara - 16, hiance - 20, dyna - 20).
+//    Колеса могуть быть заменяемы если они одинакового диаметра.
+//    Бензобак имеет атрибут - количество бензина
+
 
 }
